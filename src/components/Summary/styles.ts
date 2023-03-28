7b5fc98cce914abdcd1a24206bcc8cec3728d237
eliabyteixeira <eliaby.teixeira@gmail.com>
2023-03-27 22:41:43 -0300

    enable horizontal scroll on summary component on mobile screens

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -2,13 +2,14 @@ import styled from 'styled-components'
 
 export const SummaryContainer = styled.section`
   width: 100%;
-  max-width: 1120px;
+  max-width: 1200px;
   margin: 0 auto;
   padding: 0 1.5rem;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 2rem;
   margin-top: -5rem;
+  overflow-x: auto;
 `
 
 interface SummaryCardProps {
